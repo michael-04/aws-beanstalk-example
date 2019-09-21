7ea1e32d1aa1ebd0c1fb85ad46295a2835f13095
Michael <bmihairaul@gmail.com>
2019-09-21 23:00:22 +0100

    added getMapping for endpoint

--- src/main/java/com/mihaiblaga/aws/awselasticbeanstalkexample1/HelloController.java
+++ src/main/java/com/mihaiblaga/aws/awselasticbeanstalkexample1/HelloController.java
@@ -1,5 +1,6 @@
 package com.mihaiblaga.aws.awselasticbeanstalkexample1;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+  @GetMapping
   public  String hello(){
     return "Hello AWS";
   }
